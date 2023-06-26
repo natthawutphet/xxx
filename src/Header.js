b08e6b1553233474197b50b6b0ae3a9ad0485583
natthawutphet <xnatthawut1990@gmail.com>
2023-06-27 01:29:49 +0700

    first commit

---
+++ src/Header.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+function Header() {
+
+  return (
+    <>
+     <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="index.html">Welcome to PG Private Grab </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+             
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+
+      <header>
+      
+
+
+
+
+        <div class="cotainer">
+            <div class="img text-center mx-auto">
+            
+           <div id="btn">
+          <p id="img_a" ><img src="img/1.png" width="100%" alt=""/></p> 
+          <p id="img_b" > <img src="img/2.png" width="100%" alt=""/></p> 
+           <p id="img_c" > <img src="img/3.png" width="100%" alt=""/></p> 
+           </div>
+
+
+            </div>
+        </div>
+      </header>
+      
+    </>
+  )
+}
+export default Header
